fa8ef3b8dc087d9ef344227ebdf81b410d537b85
Can Toraman <cantoraman@gmail.com>
2019-02-08 09:22:44 +0000

    add null option to datetime on models

--- jobs/models.py
+++ jobs/models.py
@@ -4,22 +4,30 @@ from django.db import models
 class Designer(models.Model):
     designer_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.designer_name
+
 
 class Manufacturer(models.Model):
     manufacturer_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.manufacturer_name
+
 
 class Job(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     designer = models.ForeignKey(Designer, on_delete=models.CASCADE)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.SET_NULL, null=True)
     job_client = models.CharField(max_length=300)
     job_description = models.CharField(max_length=500)
-    creation_date = models.DateTimeField('creation date')
-    start_date = models.DateTimeField('start date')
-    completion_date = models.DateTimeField('completion date')
-    rejection_date = models.DateTimeField('rejection date')
+    creation_date = models.DateTimeField('creation date', null=True)
+    start_date = models.DateTimeField('start date', null=True)
+    completion_date = models.DateTimeField('completion date', null=True)
+    rejection_date = models.DateTimeField('rejection date', null=True)
     is_flagged = models.BooleanField(default=False)
     in_progress = models.BooleanField(default=False)
     is_completed = models.BooleanField(default=False)
     rejection_text = models.TextField(max_length=500)
 
+    def __str__(self):
+        return self.job_description
