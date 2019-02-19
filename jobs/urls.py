3bda0444022829c2d93dd3fca7aad7ac5aee6dc1
Can Toraman <cantoraman@gmail.com>
2019-02-19 17:27:45 +0000

    add designer list and details route

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:job_id>/', views.job_detail, name='job detail'),
+    path('title/', views.add_title, name='add title'),
+    path('designers/', views.designers_list, name='list of designers'),
+    path('designers/<int:designer_id>/', views.designer_detail, name='designer detail'),
 ]
 
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from .models import Job, Manufacturer, Designer
 
 
 def index(request):
-    return HttpResponse('In Progress')
+    active_jobs = Job.objects.filter(is_completed=False)
+    context = {
+        'active_jobs_list': active_jobs,
+    }
+    return render(request, 'jobs/index.html', context)
+
+
+def job_detail(request, job_id):
+    try:
+        job = Job.objects.get(pk=job_id)
+        some_list = ['asd', 'sdfsdf', '2312312']
+    except Job.DoesNotExist:
+        raise Http404('The job you are looking for does not exist')
+    return render(request, 'jobs/job_detail.html', {'job': job,
+                                                    'some_list': some_list})
+
+
+def add_title(request):
+    # this is a dummy view
+    return render(request, 'jobs/title.html')
+
+
+def designers_list(request):
+    designers = Designer.objects.all()
+    context = {
+        'designers': designers,
+    }
+    return render(request, 'jobs/designers.html', context)
+
+
+def designer_detail(request, designer_id):
+    designer = get_object_or_404(Designer, pk=designer_id)
+    return render(request, 'jobs/designer_details.html', {'designer': designer})
+
+
+# def manufacturers_list(request):
+#    return HttpResponse('List of Manufacturers')
+
+
+# def manufacturer_detail(request, manufacturer_id):
+#   return HttpResponse('placeholder')
