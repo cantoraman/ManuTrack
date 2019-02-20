cdffc55b987e2c921513b93e791d8e8ce16a0257
Can Toraman <cantoraman@gmail.com>
2019-02-20 10:11:46 +0000

    succesfully implement template inheritence, start applying

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'jobs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:job_id>/', views.job_detail, name='job detail'),
